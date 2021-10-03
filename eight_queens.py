f739b4a9035fd97aabffb95e8a5641a3e27a544c
marcoantonioaav <56897818+marcoantonioaav@users.noreply.github.com>
2021-10-03 20:34:42 -0300

    Oito rainhas
    Todas as funções (menos run_ga) funcionando

--- eight_queens.py
+++ eight_queens.py
@@ -1,3 +1,55 @@
+import random
+
+def verifica_linha(x, individual):
+    ataques = 0
+    
+    for i in range(len(individual)):
+        if i != x and individual[i] == individual[x]:
+            ataques += 1
+    
+    return ataques
+
+def verifica_diagonal(x, individual):
+    ataques = 0
+    
+    # sentido / baixo
+    y = individual[x]-1
+    xNovo = x-1
+    while (xNovo >= 0 and y > 0):
+        if individual[xNovo] == y:
+            ataques +=1
+        y -= 1
+        xNovo -=1
+
+    # sentido / cima
+    y = individual[x]+1
+    xNovo = x+1
+    while (xNovo < len(individual) and y < len(individual)+1):
+        if individual[xNovo] == y:
+            ataques +=1
+        y += 1
+        xNovo +=1
+
+    # sentido \ cima
+    y = individual[x]+1
+    xNovo = x-1
+    while (xNovo > 0 and y < len(individual)+1):
+        if individual[xNovo] == y:
+            ataques +=1
+        y += 1
+        xNovo -=1
+
+    # sentido \ baixo
+    y = individual[x]-1
+    xNovo = x+1
+    while (xNovo < len(individual) and y > 0):
+        if individual[xNovo] == y:
+            ataques +=1
+        y -= 1
+        xNovo +=1
+
+    return ataques
+
 def evaluate(individual):
     """
     Recebe um indivíduo (lista de inteiros) e retorna o número de ataques
@@ -7,7 +59,13 @@ def evaluate(individual):
     :param individual:list
     :return:int numero de ataques entre rainhas no individuo recebido
     """
-    raise NotImplementedError  # substituir pelo seu codigo
+    total_ataques = 0
+
+    for x in range(len(individual)):
+        total_ataques += verifica_linha(x, individual)
+        total_ataques += verifica_diagonal(x, individual)
+    
+    return total_ataques/2
 
 
 def tournament(participants):
@@ -17,7 +75,16 @@ def tournament(participants):
     :param participants:list - lista de individuos
     :return:list melhor individuo da lista recebida
     """
-    raise NotImplementedError  # substituir pelo seu codigo
+    melhor_inividuo = participants[0]
+    melhor_conflitos = 10000
+
+    for individuo in participants:
+        conflitos = evaluate(individuo)
+        if conflitos < melhor_conflitos:
+            melhor_inividuo = individuo
+            melhor_conflitos = conflitos
+
+    return melhor_inividuo
 
 
 def crossover(parent1, parent2, index):
@@ -34,7 +101,14 @@ def crossover(parent1, parent2, index):
     :param index:int
     :return:list,list
     """
-    raise NotImplementedError  # substituir pelo seu codigo
+    final1 = parent1[index:]
+    final2 = parent2[index:]
+    filho1 = parent1[:index]
+    filho1 += final2
+    filho2 = parent2[:index]
+    filho2 += final1
+
+    return filho1, filho2
 
 
 def mutate(individual, m):
@@ -46,7 +120,11 @@ def mutate(individual, m):
     :param m:int - probabilidade de mutacao
     :return:list - individuo apos mutacao (ou intacto, caso a prob. de mutacao nao seja satisfeita)
     """
-    raise NotImplementedError  # substituir pelo seu codigo
+    if random.random() < m:
+        sorteio = random.randint(0, len(individual)-1)
+        individual[sorteio] = random.randint(1,8)
+    
+    return individual
 
 
 def run_ga(g, n, k, m, e):
@@ -59,4 +137,38 @@ def run_ga(g, n, k, m, e):
     :param e:bool - se vai haver elitismo
     :return:list - melhor individuo encontrado
     """
-    raise NotImplementedError  # substituir pelo seu codigo
+    #1 Gera n individuos
+    individuos = [None]*n
+    for i in range(len(individuos)):
+        novo_individuo = [None]*8
+        for j in range(len(novo_individuo)):
+            novo_individuo[j] = random.randint(1,8)
+        individuos[i] = novo_individuo
+        
+    melhor_individuo = tournament(individuos)
+    for geração in range(g):
+        #1.1 Muta os n individuos
+        for i in range(len(individuos)-1):
+            individuos[i] = mutate(melhor_individuo, m)
+    
+        #2 Avalia os n individuos
+        #3 ordena com base na pontuação
+        melhor_individuo = tournament(individuos)
+        
+        # Se tem elitismo...
+        if e:
+            individuos.remove(melhor_individuo)
+            segundo_melhor = tournament(individuos)
+            index = random.randint(0, len(melhor_individuo)-1)
+            melhor_individuo, segundo_melhor = crossover(melhor_individuo, segundo_melhor, index)
+            individuos.append(melhor_individuo)
+        print(evaluate(melhor_individuo))
+    
+    return melhor_individuo
+
+
+if __name__ == "__main__":
+    #melhor_individuo = run_ga(100, 40, 2, 0.3, True)
+    melhor_individuo = run_ga(3, 5, 2, 0.3, False)
+    print(melhor_individuo)
+    #print(evaluate(melhor_individuo))
